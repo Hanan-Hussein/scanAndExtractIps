f4277d3c9c5de702925fc749403809019331616b
Hanan-Hussein <hanhuss035@gmail.com>
2023-05-28 17:25:24 +0300

    scan ips using mascan and extract the ips

---
+++ scan-v1.py
@@ -0,0 +1,20 @@
+import json
+import masscan
+import pyfiglet
+import json
+Banner = pyfiglet.figlet_format("block scanner")
+print(Banner)
+
+# * Defining block function
+def block_scan(ip_range, filename, ports):
+    mas = masscan.PortScanner(masscan_search_path=('masscan/bin/masscan',))  
+    mas.scan(ip_range, ports=ports, arguments='--max-rate 1000000', sudo=True)
+    with open(filename, 'w') as f:
+        f.write(mas.scan_result)
+
+
+
+block_scan('10.0.0.0/8', '10_ip_blocks.json', '80,443')
+block_scan('172.16.0.0/12', '172_ip_blocks.json', '80,443')
+block_scan('192.168.0.0/16', '192_ip_blocks.json', '80,443')
+
---
+++ trialExtract.py
@@ -0,0 +1,39 @@
+import json
+
+commonPorts = [80, 443]
+
+
+def ip_extract(json_data):
+    ip_addresses = []
+    data = json.loads(json_data)
+
+    for ip, ports in data['scan'].items():
+        for a in ports:
+
+            ip_addresses.append(f"{ip}:{a['port']}")
+    return ip_addresses
+
+
+json_files = ['192_ip_blocks.json','172_ip_blocks.json','10_ip_blocks.json']
+all_ips = []
+
+for file in json_files:
+    with open(file, 'r') as f:
+        json_data = f.read()
+
+    ips = ip_extract(json_data)
+
+    all_ips.extend(ips)
+# print(all_ips)
+for i in commonPorts:
+
+    with open('ip_addresses.txt', 'w') as extract:
+        with open(f"{i}.txt", 'w') as output_file:
+
+            for ip in all_ips:
+                extract.write(ip + "\n")
+                newIps = ip.split(':')
+                a, b = newIps
+                if int(b) == i:
+                    output_file.write(f"{a}:{b}" + '\n')
+print('ip addresses saved to ip_address.txt')
